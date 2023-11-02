6f1d134cfcf0ab99f8693bb06a1f8759ed8817a2
castromaciel <castromaciel00@gmail.com>
2023-11-01 23:49:26 -0300

    feat: add data

---
+++ src/interfaces/data.interface.ts
@@ -0,0 +1,9 @@
+import { AnimationObject } from 'lottie-react-native';
+
+export interface OnboardingData {
+  id: number;
+  animation: AnimationObject;
+  text: string;
+  textColor: string;
+  backgroundColor: string;
+}
